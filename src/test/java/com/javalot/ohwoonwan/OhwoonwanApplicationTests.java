8b2d5363bd13442674cfa26f314a63f7dcba15bd
anjaekk <anjaekk@gmail.com>
2023-02-26 14:31:34 +0900

    Fix: settings

---
+++ src/test/java/com/javalot/ohwoonwan/OhwoonwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalot.ohwoonwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhwoonwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
